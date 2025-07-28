9e4941d9a71298410dda69a3f52f689951167fe2
raj kumar <rajkumarbhootla@gmail.com>
2025-07-28 10:50:14 +0530

    upadate

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,17 @@
           playsinline
           preload="auto"
           class="absolute top-0 left-0 w-full h-[75%] object-cover scale-125"
+          @loadeddata="handleLoaded"
         ></video>
+
+        <div
+          v-if="loading"
+          class="flex justify-center items-center text-red-500 text-4xl text-center pt-8 pl-12 h-[67h]"
+        >
+          <div class="pt-46">
+            <font-awesome-icon spin icon="fa-solid fa-spinner text-red-500" />
+          </div>
+        </div>
       </div>
 
       <Trending />
@@ -20,7 +30,14 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import Trending from './Trending.vue'
 import Originals from './Originals.vue'
 import videoUrl from '@/assets/liger-trailer-1080p.mp4'
+
+const loading = ref(true)
+
+const handleLoaded = () => {
+  loading.value = false
+}
 </script>
