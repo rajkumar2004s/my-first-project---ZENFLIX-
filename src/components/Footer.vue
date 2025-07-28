d3654ef42ec0172c2163460bd4c9541af6607c27
raj kumar <rajkumarbhootla@gmail.com>
2025-07-28 15:21:45 +0530

    update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,18 @@
     <div>
       <h1 class="text-2xl text-white font-bold pl-20 t hover:text-blue-500">Contact Us</h1>
       <div class="text-white text-xl pt-4 flex gap-6 pl-16">
-        <a href="https://www.linkedin.com/in/raju03/"> <i class="fa-brands fa-linkedin"></i></a>
-        <a href="https://www.instagram.com/_raju.uu/"> <i class="fa-brands fa-instagram"></i></a>
-        <a><i class="fa-brands fa-google"></i></a>
-        <a><i class="fa-brands fa-twitter"></i></a>
+        <a href="https://www.linkedin.com/in/raju03/" target="_blank">
+          <i class="fa-brands fa-linkedin"></i
+        ></a>
+        <a href="https://www.instagram.com/_raju.uu/" target="_blank">
+          <i class="fa-brands fa-instagram"></i
+        ></a>
+        <a href="https://zenflix-ten.vercel.app/" target="_blank"
+          ><i class="fa-brands fa-google"></i
+        ></a>
+        <a href="https://x.com/RBhootla86833?t=V_qqA9THCrLt2_j1jOd1Ag&s=08 " target="_blank"
+          ><i class="fa-brands fa-twitter"></i
+        ></a>
       </div>
     </div>
   </div>
