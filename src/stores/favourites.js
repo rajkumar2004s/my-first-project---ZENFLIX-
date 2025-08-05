5ef9850b4d6e3f8a9a37b7cb6d6549f19a40b956
raj kumar <rajkumarbhootla@gmail.com>
2025-08-05 15:13:28 +0530

    Update

--- src/stores/favourites.js
+++ src/stores/favourites.js
@@ -1,8 +1,17 @@
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 export const useFavouritesStore = defineStore('favourites', () => {
-  const favourites = ref([])
+  const saved = localStorage.getItem('favourites')
+  const favourites = ref(saved ? JSON.parse(saved) : [])
+
+  watch(
+    favourites,
+    (newFavourites) => {
+      localStorage.setItem('favourites', JSON.stringify(newFavourites))
+    },
+    { deep: true },
+  )
 
   const addToFavourites = (movie) => {
     const exists = favourites.value.some((fav) => fav.id === movie.id)
